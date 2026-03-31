fc9398bbc87e7b41caf60549427728e8c6d4325f
Julie Aubry <julie.aubry26@gmail.com>
2026-03-31 12:06:38 +0200

    FEAT- Get film list from DB + Delete movie from DB with post request

--- backend/routes/movies.js
+++ backend/routes/movies.js
@@ -4,10 +4,12 @@ import Movie from '../entities/movies.js';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    console.log("Dans movies.js");
+router.get('/', async (req, res) => {
+    const movieRepository = appDataSource.getRepository(Movie);
+    const movies = await movieRepository.find();
+    console.log(movies);
     // Au lieu de la liste vide, on aurait mis la liste hardcodée de films
-    res.send([]);
+    res.send(movies);
     
 });
 
@@ -17,18 +19,36 @@ router.post('/new', (req,res) => {
     title: req.body.title,
     date: req.body.date
   });
-
   movieRepository
     .insert(newMovie)
     .then(function (newDocument) {
-      res.status(201).json(newDocument);
-      res.send("Film added to database.");
+    res.status(201).json(newDocument);
     })
     .catch(function (error) {
-      console.error(error);
+    console.error(error);
         res.status(500).json({ message: 'Error while creating the movie' });
     });
 })
+
+router.post('/delete', (req, res) => {
+  const movieRepository = appDataSource.getRepository(Movie);
+  const movieId = req.body.id;
+
+  movieRepository
+    .delete(movieId)
+    .then((result) => {
+      if (result.affected === 0) {
+        return res.status(404).json({ message: 'Movie not found' });
+      }
+      res.status(200).json({ message: 'Movie deleted successfully' });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).json({ message: 'Error while deleting the movie' });
+    });
+});
+
+
 
 export default router;
 
